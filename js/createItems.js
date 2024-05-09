51ab7bec4916349f6689aa5e86314bf3c0ec38a3
Albert <a91614830@gmail.com>
2024-05-09 18:54:53 +0200

    feature:create-createCartElementsForItems-func

--- js/createItems.js
+++ js/createItems.js
@@ -1,12 +1,13 @@
 import { items } from "./itemsList.js";
 const categoryBtn = document.querySelector(".category");
+const itemsCnt = document.querySelector(".itemsCnt");
 export class CreateItems {
-    constructor() {
+    constructor(itemsReturn) {
         this.itemsReturn = [];
         this.listenerFunc();
     }
     listenerFunc() {
-        categoryBtn === null || categoryBtn === void 0 ? void 0 : categoryBtn.addEventListener('click', (e) => { this.filteringOfItemsArray(e); });
+        categoryBtn === null || categoryBtn === void 0 ? void 0 : categoryBtn.addEventListener('click', (e) => { this.filteringOfItemsArray(e), this.createCardElementsForItems(); });
     }
     filteringOfItemsArray(e) {
         let value = e.target.value;
@@ -14,5 +15,35 @@ export class CreateItems {
         whatCategory.forEach(el => {
             this.itemsReturn.push(el);
         });
+    }
+    createCardElementsForItems() {
+        if (this.itemsReturn.length !== 0) {
+            this.itemsReturn.forEach((el) => {
+                const articleEl = document.createElement("article");
+                const h2 = document.createElement("h2");
+                const p = document.createElement("p");
+                const strong = document.createElement("strong");
+                const pCategory = document.createElement("p");
+                const inputNumber = document.createElement("input");
+                const addToCartBtn = document.createElement("button");
+                h2.textContent = el.title;
+                pCategory.textContent = el.category;
+                articleEl.classList.add("itemsCnt__item");
+                h2.classList.add("itemsCnt__itemTitle");
+                p.classList.add("itemsCnt__itemDescrition");
+                strong.classList.add("itemsCnt__itemPrice");
+                pCategory.classList.add("itemsCnt__itemTitleCategory");
+                inputNumber.classList.add("itenmsCnt__itemQuantity");
+                addToCartBtn.classList.add("itemsCnt__addToCart");
+                articleEl.appendChild(h2);
+                articleEl.appendChild(pCategory);
+                articleEl.appendChild(inputNumber);
+                articleEl.appendChild(addToCartBtn);
+                return itemsCnt === null || itemsCnt === void 0 ? void 0 : itemsCnt.appendChild(articleEl);
+            });
+        }
+        else {
+            console.log("is zero");
+        }
     }
 }
--- ts/createItems.ts
+++ ts/createItems.ts
@@ -1,19 +1,20 @@
 import { items } from "./itemsList.js";
 
-const categoryBtn = document.querySelector(".category") as HTMLElement ;
+const categoryBtn = document.querySelector(".category") as HTMLElement;
+const itemsCnt = document.querySelector(".itemsCnt");
 
 export class CreateItems {
 
-	itemsReturn: Object[];
+	public itemsReturn: Object[];
 	
-	constructor() {
+	constructor(itemsReturn: []) {
 		this.itemsReturn = [];
   
 		this.listenerFunc();
 	}
 
 	listenerFunc() {
-		categoryBtn?.addEventListener('click', (e) => { this.filteringOfItemsArray(e) });
+		categoryBtn?.addEventListener('click', (e) => { this.filteringOfItemsArray(e), this.createCardElementsForItems() });
 	}
 
 	filteringOfItemsArray(e: any) {
@@ -24,8 +25,48 @@ export class CreateItems {
 		whatCategory.forEach(el => {
 			this.itemsReturn.push(el);
 		})
-
 	}
 
+	
+
+	createCardElementsForItems() {
+		if (this.itemsReturn.length !== 0) {
+
+			this.itemsReturn.forEach((el:any) => {
+				
+				
+
+				const articleEl = document.createElement("article");
+			   const h2 = document.createElement("h2");
+				const p = document.createElement("p");
+				const strong = document.createElement("strong");
+				const pCategory = document.createElement("p");
+				const inputNumber = document.createElement("input");
+				const addToCartBtn = document.createElement("button");
+ 
+			
+
+				h2.textContent =  el.title;
+				pCategory.textContent = el.category;
+
+				articleEl.classList.add("itemsCnt__item");
+				h2.classList.add("itemsCnt__itemTitle");
+				p.classList.add("itemsCnt__itemDescrition");
+				strong.classList.add("itemsCnt__itemPrice");
+				pCategory.classList.add("itemsCnt__itemTitleCategory");
+				inputNumber.classList.add("itenmsCnt__itemQuantity");
+				addToCartBtn.classList.add("itemsCnt__addToCart");
+
+				articleEl.appendChild(h2);
+				articleEl.appendChild(pCategory);
+				articleEl.appendChild(inputNumber);
+				articleEl.appendChild(addToCartBtn);
+				return itemsCnt?.appendChild(articleEl);
+			})
+		} else {
+			console.log("is zero")
+		}
+	
+	}
 
 }
