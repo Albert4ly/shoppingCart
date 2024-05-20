c51b9f94470fffd799a14e992e012e66565a7515
Albert <a91614830@gmail.com>
2024-05-20 16:35:29 +0200

    refactor:changes

--- ts/index.ts
+++ ts/index.ts
@@ -1,8 +1,37 @@
 import { CreateItems } from "./createItems.js";
-import { LocalStorage } from "./localStorage.js";
+// import { LocalStorage } from "./localStorage.js";
 
+import { CartStorage } from "./cartStorage.js";
 
+declare global {
+   interface Window {
+      cart: CartStorage;
+   }
+}
+
+const redirectFunction = (location: string): void => {
+   window.location.hash = `#/${location}`;
+}
 // const ls = new LocalStorage();
 // ls.getItemsFromLocalStorage();
 new CreateItems();
-new LocalStorage();
+// new LocalStorage();
+
+const tshirtBtn = document.querySelector('.category__tshirtsBtn');
+
+if (tshirtBtn) {
+   tshirtBtn.addEventListener('click', () => redirectFunction('tshirts'));
+}
+
+const hoodieBtn = document.querySelector('.category__hoodiesBtn');
+
+
+if (hoodieBtn) {
+   hoodieBtn.addEventListener('click', () => redirectFunction('hoodies'));
+}
+
+const storage = new CartStorage();
+
+window.cart = storage;
+
+export { };
