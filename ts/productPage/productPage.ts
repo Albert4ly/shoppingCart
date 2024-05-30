45a7e80d62fd14c14f4561bf5d82fb7150c3bdc7
Albert <a91614830@gmail.com>
2024-05-30 18:45:33 +0200

    feat:abstract-product-page-class

---
+++ ts/productPage/productPage.ts
@@ -0,0 +1,35 @@
+import { Product } from "../product.js";
+import { Router } from "../router/router.js";
+import { Cart } from "../cart/cart.js";
+
+export abstract class ProductPage<T extends Product> {
+
+   protected readonly abstract productTemplate: (product: T) => HTMLElement;
+   protected abstract readonly products: T[] = [];
+   
+   private readonly rootElement!: HTMLDivElement;
+
+   public constructor(
+      containerId: string,
+      listingName: string,
+      private readonly router: Router,
+      protected readonly cart: Cart
+   ) {
+      const containerElement = document.getElementById(containerId);
+
+      if (!containerId) {
+         return;
+      }
+
+      this.router.addRoute({ name: listingName, renderFunc: this.render });
+      this.rootElement = containerElement as HTMLDivElement;
+   }
+   
+   public render = (): void => {
+      while (this.rootElement.firstChild) {
+         this.rootElement.firstChild.remove();
+      }
+      const productBoxes = this.products.map(this.productTemplate);
+      productBoxes.forEach(product => this.rootElement.appendChild(product));
+   };
+} 
